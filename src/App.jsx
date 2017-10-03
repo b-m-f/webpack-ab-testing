cb13a2717c9c879946ea7649116af16f89b0fc7d
bananenmannfrau <bananenmannfrau@posteo.net>
2017-10-03 16:58:09 +0200

    updates components, and adds react preset

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from './Button.jsx';
+
+export default () => 
+  <div>
+    <ul>
+      <li>
+        Item 1
+      </li>
+      <li>
+        Item 2
+      </li>
+    </ul>
+    <Button />
+  </div>
+
---
+++ src/Button.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+// import styles from 'Button.css';
+
+export default () => <button className={styles.button}>Button</button>;
